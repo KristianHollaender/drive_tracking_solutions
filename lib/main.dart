05de7731ea3c2290c30ad709e0ddcad99aa0591f
KristianHollaender <kristianhansen9@hotmail.dk>
2023-04-26 11:16:41 +0200

    Worked more on the calendar view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:drive_tracking_solutions/screens/mobile/mobile_home_screen.dart';
 import 'package:drive_tracking_solutions/screens/mobile/mobile_start_screen.dart';
-import 'package:drive_tracking_solutions/widgets/bottom_nav_bar.dart';import 'package:flutter/material.dart';
-import 'package:drive_tracking_solutions/drive_tracking_solutions.dart';
-import 'package:drive_tracking_solutions/screens/mobile/calendar_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,9 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
-      theme: ThemeData(
-      ),
-      home: const MobileApp(),
+      theme: ThemeData(),
+      home: SafeArea(top: true, child: const MobileApp()),
     );
   }
 }
--- lib/screens/mobile/mobile_overview_screen.dart
+++ lib/screens/mobile/mobile_overview_screen.dart
@@ -1,16 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class OverviewScreen extends StatelessWidget {
-  const OverviewScreen({Key? key}) : super(key: key);
+class OverviewScreen extends StatefulWidget {
+  @override
+  _OverviewScreenState createState() => _OverviewScreenState();
+}
+
+class _OverviewScreenState extends State<OverviewScreen> {
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Text(
-            "OverviewScreen",
-            style: TextStyle(fontSize: 50),
-          )),
+      appBar: AppBar(
+        title: Text('Overview'),
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: double.infinity,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                  elevation: 4.0,
+                  child: TableCalendar(
+                    headerStyle: HeaderStyle(
+                      formatButtonVisible: false,
+                      titleCentered: true,
+                    ),
+                    selectedDayPredicate: (day) {
+                      return isSameDay(_selectedDay, day);
+                    },
+                    onDaySelected: (selectedDay, focusedDay) {
+                      setState(() {
+                        _selectedDay = selectedDay;
+                        _focusedDay = focusedDay;
+                      });
+                    },
+                    focusedDay: _focusedDay,
+                    firstDay: DateTime.utc(2021, 1, 1),
+                    lastDay: DateTime.utc(2030, 12, 31),
+                    weekNumbersVisible: true,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+              flex: 1,
+              child: SingleChildScrollView(
+                child: Container(
+                  height: 400,
+                  width: double.infinity,
+                  color: Colors.red,
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
