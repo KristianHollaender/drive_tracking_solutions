57cf4cc8ebd38db627c7323190c25f9a474e7c8e
KristianHollaender <kristianhansen9@hotmail.dk>
2023-04-26 09:13:40 +0200

    Created initial folders for further work

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
