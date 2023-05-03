08d6bb9fff81f143c3b9779858ee0ff603081882
MarcusIversen <91528331+MarcusIversen@users.noreply.github.com>
2023-05-03 11:11:24 +0200

    HomeScreen (WIP)

--- lib/screens/mobile/mobile_home_screen.dart
+++ lib/screens/mobile/mobile_home_screen.dart
@@ -1,16 +1,215 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Text(
-              "HomeScreen",
-              style: TextStyle(fontSize: 50),
-          )),
+  State<HomeScreen> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  CameraPosition? _initialCameraPosition;
+  CameraPosition? _currentLocationCameraPosition;
+  LatLng? _latLng;
+  StreamSubscription<LocationData>? sub;
+  bool _click = false;
+
+  Future<void> getCurrentLocation() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+
+    _latLng = LatLng(_locationData.latitude!, _locationData.longitude!);
+
+    _initialCameraPosition = CameraPosition(
+      target: _latLng!,
+      zoom: 17.5,
     );
+
+    _currentLocationCameraPosition = CameraPosition(
+      target: _latLng!,
+      zoom: 17.5,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: getCurrentLocation(),
+      builder: (context, snapshot) {
+        if (_latLng == null || _initialCameraPosition == null) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Home"),
+          ),
+          body: Column(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Stack(
+                      children: [
+                        GoogleMap(
+                          mapType: MapType.hybrid,
+                          myLocationButtonEnabled: true,
+                          myLocationEnabled: true,
+                          compassEnabled: true,
+                          initialCameraPosition: _initialCameraPosition!,
+                          onMapCreated: (GoogleMapController controller) {
+                            _controller.complete(controller);
+                          },
+                        ),
+                        Positioned(
+                          bottom: 8.0,
+                          right: 165.0,
+                          left: 165.0,
+                          child: FloatingActionButton.extended(
+                            onPressed: () {
+                              setState(() {
+                                _click = !_click;
+                              });
+                              if (_click) {
+                                _listenToCurrentLocation();
+                              } else {
+                                _stopListeningToLocation();
+                              }
+                            },
+                            label: _click
+                                ? const Icon(Icons.navigation)
+                                : const Icon(Icons.navigation_outlined),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Center(
+                              child: Container(
+                                color: Colors.blueGrey,
+                                height: 80.0,
+                                width: double.infinity,
+                                child: const Center(
+                                  child: Text(
+                                    "00:00:00",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 62.0),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Column(
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: FloatingActionButton(
+                                      onPressed: null,
+                                  backgroundColor: Colors.red,
+                                  shape: ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _goToCurrentLocation() async {
+    getCurrentLocation();
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+        CameraUpdate.newCameraPosition(_currentLocationCameraPosition!));
+    print(_latLng);
+    print(_currentLocationCameraPosition);
+  }
+
+  LocationData? currentLocation;
+
+  void _listenToCurrentLocation() async {
+    Location location = Location();
+    location.getLocation().then(
+      (location) {
+        currentLocation = location;
+      },
+    );
+    GoogleMapController googleMapController = await _controller.future;
+    sub = location.onLocationChanged.listen(
+      (newLoc) {
+        currentLocation = newLoc;
+        googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              zoom: 17.5,
+              target: LatLng(
+                newLoc.latitude!,
+                newLoc.longitude!,
+              ),
+            ),
+          ),
+        );
+        setState(() {});
+      },
+    );
+  }
+
+  void _stopListeningToLocation() async {
+    sub!.cancel();
   }
 }
