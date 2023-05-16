90848b881b3c5de9e2662f87cb864c43182f59ec
MarcusIversen <91528331+MarcusIversen@users.noreply.github.com>
2023-05-16 16:34:26 +0200

    Update mobile_new_user_screen.dart

--- mobile_app/lib/screens/mobile/mobile_new_user_screen.dart
+++ mobile_app/lib/screens/mobile/mobile_new_user_screen.dart
@@ -1,14 +1,15 @@
-import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as path;
-import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'dart:io';
+
 import 'package:drive_tracking_solutions/logic/fire_service.dart';
 import 'package:drive_tracking_solutions/screens/mobile/mobile_login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 class MobileNewUserScreen extends StatefulWidget {
@@ -53,6 +54,15 @@ class _MobileNewUserScreenState extends State<MobileNewUserScreen> {
   Column _buildUserForm(BuildContext context) {
     return Column(
       children: [
+        Center(
+          child: Container(
+            child: Image.asset(
+              'assets/Logo-lighter.png',
+              width: MediaQuery.of(context).size.width * 0.75,
+            ),
+            height: MediaQuery.of(context).size.height * 0.25,
+          ),
+        ),
         const SizedBox(height: 16),
         Container(
           padding: const EdgeInsets.only(bottom: 16),
@@ -70,16 +80,25 @@ class _MobileNewUserScreenState extends State<MobileNewUserScreen> {
           padding: const EdgeInsets.only(bottom: 16),
           child: passwordInput(),
         ),
-        Center(
-          child: _image == null
-              ? const Text('No image selected.')
-              : Image.file(_image!, height: 300, fit: BoxFit.cover),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        Row(
           children: [
-            pickImageBtn(),
-            createUserBtn(context),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Center(
+                  child: _image == null
+                      ? const Text('No image selected.')
+                      : Image.file(_image!, height: MediaQuery.of(context).size.height * 0.27, fit: BoxFit.cover),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                pickImageBtn(),
+                createUserBtn(context),
+              ],
+            )
           ],
         )
       ],
@@ -111,51 +130,66 @@ class _MobileNewUserScreenState extends State<MobileNewUserScreen> {
     }
   }
 
-  ElevatedButton pickImageBtn() {
-    return ElevatedButton(
-      onPressed: () {
-        pickImage();
-      },
-      child: const Text('Select Image'),
+  Widget pickImageBtn() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: SizedBox(
+        width: 175,
+        child: FloatingActionButton.extended(
+          onPressed: () {
+            pickImage();
+          },
+          icon: Icon(Icons.edit),
+          label: const Text('Select Image'),
+          backgroundColor: Color(0xff26752b),
+        ),
+      ),
     );
   }
 
-  ElevatedButton createUserBtn(BuildContext context) {
+  Widget createUserBtn(BuildContext context) {
     final fireService = Provider.of<FirebaseService>(context);
-    return ElevatedButton(
-      child: const Text('Create new account'),
-      onPressed: () async {
-        if (!_formKey.currentState!.validate()) {
-          setState(() {});
-          return;
-        }
-        final firstname = _firstName.value.text.trim();
-        final lastname = _lastName.value.text.trim();
-        final email = _email.value.text.trim();
-        final password = _password.value.text.trim();
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: SizedBox(
+        width: 175,
+        child: FloatingActionButton.extended(
+          backgroundColor: Color(0xff26752b),
+          label: const Text('Create Account'),
+          onPressed: () async {
+            if (!_formKey.currentState!.validate()) {
+              setState(() {});
+              return;
+            }
+            final firstname = _firstName.value.text.trim();
+            final lastname = _lastName.value.text.trim();
+            final email = _email.value.text.trim();
+            final password = _password.value.text.trim();
 
-        await fireService.signUp(email, password, firstname, lastname).then(
-              (value) => {
-                SystemChannels.textInput.invokeListMethod('TextInput.hide'),
-              },
+            await fireService.signUp(email, password, firstname, lastname).then(
+                  (value) => {
+                    SystemChannels.textInput.invokeListMethod('TextInput.hide'),
+                  },
+                );
+
+            if (_image != null) {
+              final fileName = _auth.currentUser?.uid;
+              final firebaseStorageRef = FirebaseStorage.instance
+                  .ref()
+                  .child('profile_images')
+                  .child(fileName!);
+              firebaseStorageRef.putFile(_image!);
+            }
+
+            _showSuccessSnackBar();
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (context) => MobileLoginScreen(),
+              ),
             );
-
-        if (_image != null) {
-          final fileName = _auth.currentUser?.uid;
-          final firebaseStorageRef = FirebaseStorage.instance
-              .ref()
-              .child('profile_images')
-              .child(fileName!);
-          firebaseStorageRef.putFile(_image!);
-        }
-
-        _showSuccessSnackBar();
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => MobileLoginScreen(),
-          ),
-        );
-      },
+          },
+        ),
+      ),
     );
   }
 
@@ -172,7 +206,24 @@ class _MobileNewUserScreenState extends State<MobileNewUserScreen> {
     return TextFormField(
       keyboardType: TextInputType.name,
       controller: _firstName,
-      decoration: const InputDecoration(label: Text('First Name')),
+      style: const TextStyle(fontSize: 18),
+      decoration: const InputDecoration(
+        labelText: 'First Name',
+        labelStyle: TextStyle(fontSize: 20, color: Colors.white),
+        prefixIcon: Icon(Icons.person, size: 28),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.green, width: 2.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 1.0),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 2.0),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.0),
+        ),
+      ),
       validator: (value) => (value == null) ? 'First name required' : null,
     );
   }
@@ -181,7 +232,24 @@ class _MobileNewUserScreenState extends State<MobileNewUserScreen> {
     return TextFormField(
       keyboardType: TextInputType.name,
       controller: _lastName,
-      decoration: const InputDecoration(label: Text('Last Name')),
+      style: const TextStyle(fontSize: 18),
+      decoration: const InputDecoration(
+        labelText: 'Last name',
+        labelStyle: TextStyle(fontSize: 20, color: Colors.white),
+        prefixIcon: Icon(Icons.person, size: 28),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.green, width: 2.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 1.0),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 2.0),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.0),
+        ),
+      ),
       validator: (value) => (value == null) ? 'Last name required' : null,
     );
   }
@@ -190,7 +258,24 @@ class _MobileNewUserScreenState extends State<MobileNewUserScreen> {
     return TextFormField(
       keyboardType: TextInputType.emailAddress,
       controller: _email,
-      decoration: const InputDecoration(label: Text('Email')),
+      style: const TextStyle(fontSize: 18),
+      decoration: const InputDecoration(
+        labelText: 'Email',
+        labelStyle: TextStyle(fontSize: 20, color: Colors.white),
+        prefixIcon: Icon(Icons.email, size: 28),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.green, width: 2.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 1.0),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 2.0),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.0),
+        ),
+      ),
       validator: (value) =>
           (value == null || !value.contains("@")) ? 'Email required' : null,
     );
@@ -199,7 +284,24 @@ class _MobileNewUserScreenState extends State<MobileNewUserScreen> {
   Widget passwordInput() {
     return TextFormField(
       controller: _password,
-      decoration: const InputDecoration(label: Text('Password')),
+      style: const TextStyle(fontSize: 18),
+      decoration: const InputDecoration(
+        labelText: 'Password',
+        labelStyle: TextStyle(fontSize: 20, color: Colors.white),
+        prefixIcon: Icon(Icons.lock, size: 28),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.green, width: 2.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 1.0),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 2.0),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.0),
+        ),
+      ),
       obscureText: true,
       validator: (value) => (value == null || value.length < 6)
           ? 'Password required (min 6 chars)'
