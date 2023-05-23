4c7d16837f06804f00ae3c7aa77a8eab28f5d198
KristianHollaender <kristianhansen9@hotmail.dk>
2023-05-23 10:30:38 +0200

    Profile page mostly done - Need to work on local storage

--- frontend/src/app/profile-page/profile-page.component.ts
+++ frontend/src/app/profile-page/profile-page.component.ts
@@ -1,22 +1,41 @@
 import {Component, OnInit} from '@angular/core';
 import {FireService} from "../fire.service";
+import {FormControl, FormGroup} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss']
 })
-export class ProfilePageComponent implements OnInit{
+export class ProfilePageComponent implements OnInit {
 
-  email: string = "";
-  firstName: string = "";
-  lastName: string = "";
-
-  constructor(private fireService: FireService) {
+  constructor(public fireService: FireService, private snackBar: MatSnackBar) {
   }
 
   async ngOnInit() {
-    const user = this.fireService.auth.currentUser;
+
   }
 
+
+  async editProfile() {
+    const id = this.fireService.user.uid;
+    let dto = {
+      email: this.fireService.user.email,
+      firstname: this.fireService.user.firstname,
+      lastname: this.fireService.user.lastname,
+    };
+
+    try {
+      await this.fireService.editUser(id, dto);
+      this.snackBar.open('User profile updated successfully', 'Close', {
+        duration: 3000,
+      });
+    } catch (error) {
+      this.snackBar.open('User profile update failed', 'Close', {
+        duration: 3000,
+      })
+      console.error('Error editing user:', error);
+    }
+  }
 }
