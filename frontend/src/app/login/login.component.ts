84bc702dd9fc3f23ccd25b58f6a9458e2b6c9753
KristianHollaender <kristianhansen9@hotmail.dk>
2023-05-25 10:00:18 +0200

    ForgotPassword works

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -10,8 +10,8 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 })
 
 export class LoginComponent {
-  email: string = "";
-  password: string = "";
+  email!: string;
+  password!: string;
 
   constructor(public fireService: FireService, private router: Router, private snackBar: MatSnackBar) {
   }
@@ -25,6 +25,7 @@ export class LoginComponent {
           // @ts-ignore
           localStorage.setItem('uid', await this.fireService.auth.currentUser?.uid)
           await this.router.navigate(['admin/users']);
+          this.snackBar.open('SUCCESSFUL LOGIN', 'Close', {duration: 3000})
         } else {
           this.snackBar.open('You are not an admin!!', 'Close', {duration: 3000})
         }
@@ -33,4 +34,17 @@ export class LoginComponent {
         console.log(error);
       });
   };
+
+  async forgotPassword(email: string) {
+    if (email != null) {
+      await this.fireService.forgotPassword(email).then(async () => {
+        this.snackBar.open('An email has been sent to: ' + email, 'Close', {duration: 3000})
+      }).catch(() => {
+        this.snackBar.open('The email you entered doesn\'t exist', 'Close', {duration: 3000})
+      });
+    } else {
+      this.snackBar.open('Please enter an email', 'Close', {duration: 3000})
+    }
+
+  }
 }
